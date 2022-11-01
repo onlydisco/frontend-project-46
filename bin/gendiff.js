4421ceb55c05384de47c2ea297d7ded800af91e0
Сергей Мухин <onlydisco@yandex.ru>
2022-11-01 10:30:52 +0500

    Updated: gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ import { program } from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>', 'Path to file #1')
+  .argument('<filepath2>', 'Path to file #2')
   .parse(process.argv);
